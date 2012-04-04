202b4a95919717581d86c72b885f5b6d45e06810
Demur Rumed <gunkmute@gmail.com>
2012-04-04 17:30:41 +0000

    Grid indicates turn. Double size. Tighten code

--- oo.c
+++ oo.c
@@ -10,18 +10,14 @@
 #define MotionNotify SDL_MOUSEMOTION
 #define EV(y) ev.y
 #endif
-#include <stdlib.h>
 #include <stdint.h>
 #include <string.h>
-#include <time.h>
-const unsigned char wht[]={255,255,255},blk[]={0,0,0},gry[]={160,160,160},sky[]={192,192,240};
-#define case(x) break;case x:;
-struct game{
-	union{char p[8];uint64_t p8;};
-	union{char e[8];uint64_t e8;};
+const unsigned char wht[]={255,255,255},blk[]={0,0,0},gry[]={160,160,160};
+typedef struct game{
+	uint64_t p,e;
 	uint8_t t;
-}G;
-typedef struct game game;
+}game;
+game G;
 int getz(uint64_t w,int z){
 	return !!(w&1LL<<z);
 }
@@ -32,66 +28,57 @@ void flipxy(uint64_t*w,int x,int y){
 	flipz(w,x+y*8);
 }
 int getpxy(game*g,int x,int y){
-	return getz(g->p8,x+y*8);
+	return getz(g->p,x+y*8);
 }
 int getexy(game*g,int x,int y){
-	return getz(g->e8,x+y*8);
-}
-int gettxy(game*g,int x,int y){
-	return !getexy(g,x,y)?0:getpxy(g,x,y)?1:-1;
+	return getz(g->e,x+y*8);
 }
 int sane(unsigned x,unsigned y){
 	return x<8&&y<8;
 }
-int trymove_(game*g,int x,int y,int d){
-	int xd,yd,v=0;
-	switch(d){
-	case(0)xd=1;yd=0;
-	case(1)xd=1;yd=1;
-	case(2)xd=0;yd=1;
-	case(3)xd=-1;yd=1;
-	case(4)xd=-1;yd=0;
-	case(5)xd=-1;yd=-1;
-	case(6)xd=0;yd=-1;
-	case(7)xd=1;yd=-1;
-	}
-	for(int i=1;;i++){
-		int xx=x+xd*i,yy=y+yd*i;
-		if(!sane(xx,yy))return 0;
-		if(!getexy(g,xx,yy))return 0;
+int trymove_(game*g,int x,int y,int xd,int yd){
+	int xx=x,yy=y;
+	for(;;){
+		xx+=xd;
+		yy+=yd;
+		if(!sane(xx,yy)||!getexy(g,xx,yy))return 0;
 		if(getpxy(g,xx,yy)==g->t){
-			if(i==1)return 0;
-			for(int j=1;j<i;j++)
-				flipxy(&g->p8,x+xd*j,y+yd*j);
-			return 1;
+			if(x+xd==xx&&y+yd==yy)return 0;
+			for(;;){
+				xx-=xd;
+				yy-=yd;
+				if(xx==x&&yy==y)return 1;
+				flipxy(&g->p,xx,yy);
+			}
 		}
 	}
 }
 void trymove(game*g,int x,int y){
 	if(getexy(g,x,y))return;
 	int v=0;
-	for(int d=0;d<8;d++)
-		v|=trymove_(g,x,y,d);
+	for(int xd=-1;xd<2;xd++)
+		for(int yd=-1;yd<2;yd++)
+			if(xd||yd)v|=trymove_(g,x,y,xd,yd);
 	if(v){
-		flipxy(&g->e8,x,y);
-		if(g->t)g->p8|=1LL<<x+y*8;
+		flipxy(&g->e,x,y);
+		if(g->t)g->p|=1LL<<x+y*8;
 		g->t=!g->t;
 	}
 }
 int main(int argc,char**argv){
 	#ifndef GLX
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Surface*dpy=SDL_SetVideoMode(128,128,0,SDL_OPENGL);
+	SDL_Surface*dpy=SDL_SetVideoMode(256,256,0,SDL_OPENGL);
 	#else
 	Display*dpy=XOpenDisplay(0);
 	XVisualInfo*vi=glXChooseVisual(dpy,DefaultScreen(dpy),(int[]){GLX_DOUBLEBUFFER,GLX_RGBA,None});
-	Window Wdo=XCreateWindow(dpy,RootWindow(dpy,vi->screen),0,0,128,128,0,vi->depth,InputOutput,vi->visual,CWColormap|CWEventMask,(XSetWindowAttributes[]){{.colormap=XCreateColormap(dpy,RootWindow(dpy,vi->screen),vi->visual,AllocNone),.event_mask=ButtonPressMask}});
+	Window Wdo=XCreateWindow(dpy,RootWindow(dpy,vi->screen),0,0,256,256,0,vi->depth,InputOutput,vi->visual,CWColormap|CWEventMask,(XSetWindowAttributes[]){{.colormap=XCreateColormap(dpy,RootWindow(dpy,vi->screen),vi->visual,AllocNone),.event_mask=ButtonPressMask}});
 	XMapWindow(dpy,Wdo);
 	glXMakeCurrent(dpy,Wdo,glXCreateContext(dpy,vi,0,GL_TRUE));
 	#endif
-	glOrtho(0,128,128,0,1,-1);
-	G.e8=103481868288;
-	G.p8=68853694464;
+	glOrtho(0,256,256,0,1,0);
+	G.e=103481868288;
+	G.p=68853694464;
 	for(;;){
 		#ifdef GLX
 		glXSwapBuffers(dpy,Wdo);
@@ -104,20 +91,18 @@ int main(int argc,char**argv){
 		while(SDL_PollEvent(&ev)){
 			if(ev.type==SDL_QUIT)return 0;
 		#endif
-			if(ev.type==ButtonPress){
-				int q=EV(button.x)>>4|EV(button.y)&240,x=q&15,y=q>>4;
-				trymove(&G,x,y);
-			}
+			if(ev.type==ButtonPress)
+				trymove(&G,EV(button.x)>>5,EV(button.y)>>5);
 		}
 		glClear(GL_COLOR_BUFFER_BIT);
 		for(int z=0;z<64;z++){
 			int x=z&7,y=z>>3;
-			glColor3ubv(!getz(G.e8,z)?gry:getz(G.p8,z)?wht:blk);
-			glRecti(x*16,y*16,x*16+16,y*16+16);
+			glColor3ubv(!getz(G.e,z)?gry:getz(G.p,z)?wht:blk);
+			glRecti(x*32,y*32,x*32+32,y*32+32);
 		}
-		glColor3ubv(sky);
+		glColor3ubv(G.t?wht:blk);
 		glBegin(GL_LINES);
-		for(int x=16;x<128;x+=16){
+		for(int x=32;x<256;x+=32){
 			glVertex2i(x,0);
 			glVertex2i(x,256);
 			glVertex2i(0,x);
